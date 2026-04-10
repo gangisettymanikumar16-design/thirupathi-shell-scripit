cacbc98f2b03f236f9889a3381efb7d80da4b4f5
.email.com <gangisettymani98@gmail.com>
2026-04-10 18:36:17 +0530

    i have created in new file

---
+++ 03-vairablenumber.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+num=20
+if ["$num" -ge 10];then
+if ["$num" -le 15];then
+ echo "the number is within range. "
+else
+ echo "the number is above range."
+fi
+else
+ echo "the number is below range."
+fi
