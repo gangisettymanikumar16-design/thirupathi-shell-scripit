c2d4cb0767311a7aaf942d12aee88fcfad57becf
.email.com <gangisettymani98@gmail.com>
2026-04-10 18:54:32 +0530

    i have created by another file

---
+++ 04-coditionstatement.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+num=85
+if ["$grade" -ge 90];then
+ echo "the grade A:"
+elif["$grade" -ge 80];then
+ echo "the grade B :"
+fi
+elif["$grade" -ge 70];then
+ echo "the grade C :"
+ echo "the grade f :"
+fi
+
